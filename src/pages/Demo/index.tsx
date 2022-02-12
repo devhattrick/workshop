dc2ed307e50a4dd3f8e3c355fba00c5ce2ad1287
devhattrick <peerapol.panmuang@gmail.com>
2022-02-12 21:15:46 +0700

    add demo

--- src/pages/Demo/index.tsx
+++ src/pages/Demo/index.tsx
@@ -1,12 +1,66 @@
 import React from 'react';
+import styles from './index.module.scss'
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Demo: React.FC = ():JSX.Element =>{
+    const navigate = useNavigate();
+
     return(
         <>
-            <div>
+            <div className={styles.container}>
                 <nav>
-                    <p>Demo</p>
+                    <p className={styles.text}>Demo</p>
                 </nav>
+                <section>
+                    <Accordion>
+                        <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                        >
+                        <Typography>Accordion 1</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        </Typography>
+                        </AccordionDetails>
+                        </Accordion>
+                        <Accordion>
+                            <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                            >
+                            <Typography>Accordion 2</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion disabled>
+                        <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel3a-content"
+                        id="panel3a-header"
+                        >
+                        <Typography>Disabled Accordion</Typography>
+                        </AccordionSummary>
+                    </Accordion>
+                </section>
+                <footer>
+                    <Button variant="contained" onClick={() => navigate(-1)}>Go Back</Button>
+                </footer>
             </div>
         </>
     )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = ():JSX.Element =>{
+    const navigate = useNavigate();
+    const myDate:any  = new Date()  //Date().toLocaleString()
+    const dateThai = myDate.toLocaleDateString('th-TH', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
     return(
         <>
             <div>
                 <nav>
-                    <p>Home</p>
+                    <p>{`Home${dateThai}`}</p>
+                    <Button variant="contained" onClick={() => navigate('/demo')}>Demo</Button>
+
+
                 </nav>
             </div>
         </>
