b3768471a5c98f42dc0cfd3f6c83f20e5c1b612f
devhattrick <peerapol.panmuang@gmail.com>
2022-02-14 22:47:08 +0700

    input list

--- src/pages/WorkShops/TodoList/index.tsx
+++ src/pages/WorkShops/TodoList/index.tsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styles from './index.module.scss'
 
 
-const NameComponent: React.FC = (): JSX.Element => {
+const TodoList: React.FC = (): JSX.Element => {
+  const [data,setData]= useState('')
+  const [list,setList]=useState<any[]>([])
+  const handleChange=(e:any)=>{
+    setData(e.target.value)
+  }
+  const submitData=(e:any)=>{
+      e.preventDefault()
+      console.log('data',data)
+      const newItem = {
+        
+        title:data,
+      }
+      setList([...list,newItem])
+      setData('')
+  }
+
   return (
     <>
-      <div>
-        <p>Prifile</p>
+      <div className={styles.containerTodoList}>
+        <p className={styles.textTopic}>Todo List {data}</p>
+        <form className={styles.from} onSubmit={submitData}>
+          <section className={styles.inputWrapper}>
+            <input type="text" className={styles.input} onChange={handleChange}
+              value={data}/>
+            <button type="submit" className={styles.btnAdd}>Add Data</button>
+          </section>
+          <section className={styles.showList}></section>
+        </form>
       </div>
     </>
   )
 }
-export default NameComponent
+export default TodoList
