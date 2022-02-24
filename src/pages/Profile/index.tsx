7194ef0aea80faf2357bda6ac61394b0dfb8e328
devhattrick <peerapol.panmuang@gmail.com>
2022-02-24 19:40:55 +0700

    add keyFrames first

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -7,15 +7,24 @@ const NameComponent: React.FC = (): JSX.Element => {
   return (
     <>
       <div className={styles.container}>
-        <p>Profile</p>
+        <p>
+          {new Date().toLocaleDateString("th-TH", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          }) + ""}
+        </p>
         <section className={styles.profileWrapper}>
           <div className={styles.profileImage}>
             <img src={Profile1} className={styles.image} alt="profile img" />
           </div>
           <p className={styles.textName}>Peerapol Panmuang</p>
         </section>
-        <section></section>
-        <section></section>
+        <section className={styles.rowWrapper}>
+          <div></div>
+          <hr className={styles.line} />
+          <div></div>
+        </section>
       </div>
     </>
   );
