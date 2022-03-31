31f7e11c70039b722859e7e0268913c53133757b
devhattrick <peerapol.panmuang@gmail.com>
2022-03-31 20:02:27 +0700

    pagination is done

--- src/components/CardData/index.tsx
+++ src/components/CardData/index.tsx
@@ -1,3 +1,4 @@
+import { style } from "@mui/system";
 import React, { useState,useEffect } from "react";
 import styles from "./index.module.scss";
 
@@ -10,8 +11,8 @@ const CardData: React.FC<Props> = (props:Props): JSX.Element => {
 // console.log('props pagination',props)
   return (
     <div className={styles.container}>
-        <section>
-            <img src={props.image} alt="pokemon"/>
+        <section className={styles.imageContainer}>
+            <img className={styles.image} src={props.image} alt="pokemon"/>
             {/* <p>{props.index+1}</p> */}
         </section>
         <section>
--- src/pages/WorkShops/Pagination/index.tsx
+++ src/pages/WorkShops/Pagination/index.tsx
@@ -1,54 +1,78 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import styles from "./index.module.scss";
 import axios from "axios";
-
+import MenuData from './menuData'
 //conponents
-import {CardData} from '../../../components'
+import { CardData } from "../../../components";
 //Api
-const pokemonData = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=200"
-const Pagination :React.FC=():JSX.Element => {
-// State
-	const [dataA,setDataA] =useState('Hello')
-	const [dataPokemon,setDatapokemon]=useState<any>(null)
+const pokemonData = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=200";
+const Pagination: React.FC = (): JSX.Element => {
+  // State
+//   const [dataA, setDataA] = useState("Hello");
+  const [dataPokemon, setDatapokemon] = useState<any>(MenuData); //* data from api
 
-	useEffect(()=>{
-		axios.get(pokemonData).then((res) => {
-			// console.log('pokemon  Data',res.data.results)
-			setDatapokemon(res.data.results)
-			// res.data.results.map((e:any)=>console.log('name',e.name))
-		})
-	},[])
+  const [dataInPage, setDataInPage] = useState<any>([]); //* list in page
+  const [page,setPage] = useState(0) //* page
+  console.log('isPage',page)
 
-	//todo ข้อมูล 10 จำนวน
-	//todo perpage
-	//? จำนวนเลขหน้า = ข้อมูลทั้งหมด / จำนวนรายการแต่ละหน้า
-	//? 10 รายการ 10/5 = 2 หน้า
-	const pagination=() => {
-		const perpageList = 5 //แสดง 5 รายการต่อ 1 หน้า
-		const pages = dataPokemon && Math.ceil(dataPokemon.length / perpageList)
-		console.log('numPage',pages)
-		const newList = Array.from({length:pages},(data,index)=>{
-			const start = index * perpageList // [0] ,[1-5]
-			return dataPokemon.slice(start,start+perpageList)
-			// console.log()
-		})
-			console.log('new List', newList)
-		
-	}
-	useEffect(() => {
-		
-		pagination()
-	},[dataPokemon])
-return(
-	<>
-		 <div>
-				<h1>{dataA}</h1>
-				<div className={styles.cardWrapper}>
-				{dataPokemon?dataPokemon.map((element:any,index:number)=><CardData name={element.name} image={element.url} />):''}
-				</div>
-				
-		 </div>
-	</>
-	)
+//   useEffect(() => {
+//     axios.get(pokemonData).then((res) => {
+//       // console.log('pokemon  Data',res.data.results)
+//       setDatapokemon(res.data.results);
+//       // res.data.results.map((e:any)=>console.log('name',e.name))
+//     });
+//   }, []);
+
+  //todo ข้อมูล 10 จำนวน
+  //todo perpage
+  //? จำนวนเลขหน้า = ข้อมูลทั้งหมด / จำนวนรายการแต่ละหน้า
+  //? 10 รายการ 10/5 = 2 หน้า
+  const pagination = () => {
+    const perpageList = 5; //แสดง 5 รายการต่อ 1 หน้า
+    const pages = MenuData && Math.ceil(MenuData.length / perpageList);
+
+    //* แบ่งช่วงข้อมูล
+    const newList = Array.from({ length: pages }, (data, index) => {
+      const start = index * perpageList; // [0] ,[1-5]
+      return MenuData.slice(start, start + perpageList);
+    });
+    return newList;
+  };
+
+  //* set เลข หน้า
+  const handlePage =(index:any) => {
+	setPage(index)
 }
-export default Pagination
+  useEffect(() => {
+    const paginate = pagination()
+    setDataInPage(paginate)
+	setDatapokemon(paginate[page])
+  }, [page]);
+
+
+console.log('dataPokemon',MenuData)
+  return (
+    <>
+      <div>
+        {/* <h1>{dataA}</h1> */}
+        <div className={styles.cardWrapper}>
+          {dataPokemon &&
+            dataPokemon.map((element: any, index: number) => (
+              <CardData name={element.name} image={element.image_url} />
+            ))}
+        </div>
+		<br />
+        <div className={styles.paginationContainer}>
+          {dataInPage.map((data: any, index: number) => {
+            return (
+              <button key={index} className={`${index === page?styles.paginationButtonActive:styles.paginationButton}`} onClick={()=>handlePage(index)}>
+                <p className={styles.textBtnNumber}> {index + 1}</p>
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Pagination;
